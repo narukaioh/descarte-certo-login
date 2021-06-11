42d11c6f5fa1f327da4df0b5ba87b4cd7a7c0217
Ju Dantas <jucienyds@gmail.com>
2021-06-11 11:33:08 -0300

    remove password validation

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
+const secret = "123abc"
 const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 4000
 
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-const createToken = (user) => jwt.sign(user, process.env.SECRET, { expiresIn: 300 })
+const createToken = (user) => jwt.sign(user, secret, { expiresIn: 300 })
 
 app.get('/', (req, res) => {
   res.send("está funcionando!")
@@ -15,16 +16,11 @@ app.get('/', (req, res) => {
 
 app.post('/login', (request, response) => {
   let { email, password } = request?.body || request?.query || request?.params
-
-  if (password === "abc123") {
-    response.send({
-      user: { email },
-      token: createToken({ email, password})
-    })
-  } else {
-    response.statusCode(401)
-  }
-
+  
+  response.send({
+    user: { email },
+    token: createToken({ email, password })
+  })
 })
 
 app.listen(port, () => {
