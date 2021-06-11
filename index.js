3fbe798d305e67a5b06b3e9cdd1002bde213f3d4
Ju Dantas <jucienyds@gmail.com>
2021-06-11 10:44:40 -0300

    added verification of password

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
-const secret = "abc123456"
 const port = process.env.PORT || 4000
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
-const createToken = (user) => jwt.sign(user, secret, { expiresIn: 300 })
+const createToken = (user) => jwt.sign(user, process.env.SECRET, { expiresIn: 300 })
 
 app.get('/', (req, res) => {
   res.send("está funcionando!")
 })
 
-app.post('/login', (req, res) => {
-  let { email, password } = req?.body || req?.query || req?.params
-  console.log(req.body)
-  res.send({
-    user: { email },
-    token: createToken({ email, password})
-  })
+app.post('/login', (request, response) => {
+  let { email, password } = request?.body || request?.query || request?.params
+
+  if (password === "abc123") {
+    response.send({
+      user: { email },
+      token: createToken({ email, password})
+    })
+  } else {
+    response.statusCode(401)
+  }
+
 })
 
 app.listen(port, () => {
